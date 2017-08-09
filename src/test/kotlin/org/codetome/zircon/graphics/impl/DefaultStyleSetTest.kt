24f2b9879b3a9f141f787ff354b7482bfeaa2906
adam-arold <arold.adam@gmail.com>
2017-08-10 01:10:26 +0200

    style set test

--- src/test/kotlin/org/codetome/zircon/graphics/impl/DefaultStyleSetTest.kt
+++ src/test/kotlin/org/codetome/zircon/graphics/impl/DefaultStyleSetTest.kt
@@ -1,9 +1,12 @@
 package org.codetome.zircon.graphics.impl
 
+import org.assertj.core.api.Assertions.assertThat
+import org.codetome.zircon.ANSITextColor
+import org.codetome.zircon.Modifier.BLINK
+import org.codetome.zircon.Modifier.CROSSED_OUT
 import org.codetome.zircon.graphics.style.DefaultStyleSet
 import org.junit.Before
 import org.junit.Test
-import org.mockito.MockitoAnnotations
 
 class DefaultStyleSetTest {
 
@@ -11,15 +14,21 @@ class DefaultStyleSetTest {
 
     @Before
     fun setUp() {
-        MockitoAnnotations.initMocks(this)
-
         target = DefaultStyleSet()
     }
 
     @Test
     fun test() {
-
     }
 
+    companion object {
+        val EXPECTED_BG_COLOR = ANSITextColor.YELLOW
+        val EXPECTED_FG_COLOR = ANSITextColor.CYAN
+        val EXPECTED_MODIFIERS = setOf(CROSSED_OUT, BLINK)
 
+        val OTHER_STYLE = DefaultStyleSet(
+                foregroundColor = EXPECTED_FG_COLOR,
+                backgroundColor = EXPECTED_BG_COLOR,
+                modifiers = EXPECTED_MODIFIERS)
+    }
 }
