ececf7b6bf986dabe0df7d8276459c81efe21eb4
adam-arold <arold.adam@gmail.com>
2017-08-10 00:34:48 +0200

    adding terminal tests

--- src/test/kotlin/org/codetome/zircon/screen/TerminalScreenTest.kt
+++ src/test/kotlin/org/codetome/zircon/screen/TerminalScreenTest.kt
@@ -1,7 +1,14 @@
 package org.codetome.zircon.screen
 
+import org.assertj.core.api.Assertions.assertThat
+import org.codetome.zircon.Position
+import org.codetome.zircon.TextCharacter
+import org.codetome.zircon.builder.TextCharacterBuilder
+import org.codetome.zircon.builder.TextImageBuilder
+import org.codetome.zircon.input.KeyStroke
 import org.codetome.zircon.terminal.Size
 import org.codetome.zircon.terminal.virtual.DefaultVirtualTerminal
+import org.codetome.zircon.terminal.virtual.TextCharacterBuffer
 import org.junit.Before
 import org.junit.Test
 import org.mockito.MockitoAnnotations
@@ -9,22 +16,87 @@ import org.mockito.MockitoAnnotations
 class TerminalScreenTest {
 
     lateinit var target: TerminalScreen
+    val terminal = DefaultVirtualTerminal(SIZE)
 
     @Before
     fun setUp() {
         MockitoAnnotations.initMocks(this)
 
-        target = TerminalScreen(TERMINAL)
+        target = TerminalScreen(terminal)
     }
 
     @Test
-    fun test() {
+    fun shouldDrainInputQueueWhenClosed() {
+        target.addInput(KeyStroke(
+                character = 'x'
+        ))
+        target.close()
+        assertThat(target.pollInput().isPresent)
+                .isFalse()
+    }
 
+    @Test
+    fun shouldResizeOnResize() {
+        val expectedSize = Size(5, 5)
+        terminal.setSize(expectedSize)
+        assertThat(terminal.getBoundableSize()).isEqualTo(expectedSize)
+    }
+
+    @Test
+    fun shouldNotBeDrawnWhenCharacterSetButNotRefreshed() {
+        val graphics = target.newTextGraphics()
+        graphics.setCharacter(Position.OFFSET_1x1, CHAR)
+
+        assertThat(target.getBackCharacter(Position.OFFSET_1x1))
+                .isEqualTo(CHAR)
+        assertThat(target.getFrontCharacter(Position.OFFSET_1x1))
+                .isNotEqualTo(CHAR)
+
+    }
+
+    @Test
+    fun shouldBeDrawnWhenCharacterSetAndRefreshed() {
+        val graphics = target.newTextGraphics()
+        graphics.setCharacter(Position.OFFSET_1x1, CHAR)
+
+        target.refresh()
+
+        assertThat(target.getBackCharacter(Position.OFFSET_1x1))
+                .isEqualTo(CHAR)
+        assertThat(target.getFrontCharacter(Position.OFFSET_1x1))
+                .isEqualTo(CHAR)
+
+    }
+
+    @Test
+    fun shouldBeAbleToDrawImageWhenDrawImageIsCalledOnNewGraphics() {
+        val graphics = target.newTextGraphics()
+        val image = TextImageBuilder.newBuilder()
+                .filler(CHAR)
+                .build()
+
+        graphics.drawImage(Position.OFFSET_1x1, image)
+
+        assertThat(target.getBackCharacter(Position.OFFSET_1x1))
+                .isEqualTo(CHAR)
+    }
+
+    @Test
+    fun shouldClearProperlyWhenClearIsCalled() {
+        target.setCharacter(Position.OFFSET_1x1, CHAR)
+        target.display()
+
+        target.clear()
+
+        assertThat(target.getBackCharacter(Position.OFFSET_1x1))
+                .isNotEqualTo(CHAR)
     }
 
 
     companion object {
         val SIZE = Size(10, 10)
-        val TERMINAL = DefaultVirtualTerminal(SIZE)
+        val CHAR = TextCharacterBuilder.newBuilder()
+                .character('x')
+                .build()
     }
 }
---
+++ src/test/kotlin/org/codetome/zircon/terminal/AbstractTerminalTest.kt
@@ -0,0 +1,60 @@
+package org.codetome.zircon.terminal
+
+import org.assertj.core.api.Assertions.assertThat
+import org.codetome.zircon.terminal.virtual.DefaultVirtualTerminal
+import org.junit.Before
+import org.junit.Test
+import org.mockito.MockitoAnnotations
+
+class AbstractTerminalTest {
+
+    lateinit var target: AbstractTerminal
+
+    @Before
+    fun setUp() {
+        MockitoAnnotations.initMocks(this)
+
+        target = DefaultVirtualTerminal()
+    }
+
+    @Test
+    fun shouldAddResizeListenerWhenAddIsCalled() {
+        var resized = false
+        target.addResizeListener(object: TerminalResizeListener {
+            override fun onResized(terminal: Terminal, newSize: Size) {
+                resized = true
+            }
+        })
+        target.setSize(Size(5, 5))
+        assertThat(resized).isTrue()
+    }
+
+    @Test
+    fun shouldNotResizeWhenSizeIsTheSame() {
+        var resized = false
+        target.setSize(Size(5, 5))
+        target.addResizeListener(object: TerminalResizeListener {
+            override fun onResized(terminal: Terminal, newSize: Size) {
+                resized = true
+            }
+        })
+        target.setSize(Size(5, 5))
+        assertThat(resized).isFalse()
+    }
+
+    @Test
+    fun shouldRemoveListenerWhenRemoveisCalled() {
+        var resized = false
+        val listener = object: TerminalResizeListener {
+            override fun onResized(terminal: Terminal, newSize: Size) {
+                resized = true
+            }
+        }
+        target.addResizeListener(listener)
+        target.removeResizeListener(listener)
+        target.setSize(Size(5, 5))
+        assertThat(resized).isFalse()
+    }
+
+
+}
