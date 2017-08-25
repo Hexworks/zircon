c37dc1fd047511b618c1954f1190c20fb18e0ddf
adam-arold <arold.adam@gmail.com>
2017-08-26 00:40:06 +0200

    fixing tileset colorizing bug

--- src/main/kotlin/org/codetome/zircon/internal/font/transformer/Java2DFontRegionCloner.kt
+++ src/main/kotlin/org/codetome/zircon/internal/font/transformer/Java2DFontRegionCloner.kt
@@ -9,8 +9,8 @@ class Java2DFontRegionCloner : FontRegionTransformer<BufferedImage> {
     override fun transform(region: BufferedImage, textCharacter: TextCharacter): BufferedImage {
         return BufferedImage(region.width, region.height, BufferedImage.TRANSLUCENT).let { clone ->
             clone.graphics.apply {
-                color = textCharacter.getBackgroundColor().toAWTColor()
-                fillRect(0, 0, region.width, region.height)
+//                color = textCharacter.getBackgroundColor().toAWTColor()
+//                fillRect(0, 0, region.width, region.height)
                 drawImage(region, 0, 0, null)
                 dispose()
             }
--- src/main/kotlin/org/codetome/zircon/internal/font/transformer/Java2DFontRegionColorizer.kt
+++ src/main/kotlin/org/codetome/zircon/internal/font/transformer/Java2DFontRegionColorizer.kt
@@ -11,16 +11,20 @@ class Java2DFontRegionColorizer : FontRegionTransformer<BufferedImage> {
         val g = textCharacter.getForegroundColor().getGreen().toFloat() / 255
         val b = textCharacter.getForegroundColor().getBlue().toFloat() / 255
 
-        (0..region.width - 1).forEach { i ->
-            (0..region.height - 1).forEach { j ->
-                val ax = region.colorModel.getAlpha(region.raster.getDataElements(i, j, null))
-                var rx = region.colorModel.getRed(region.raster.getDataElements(i, j, null))
-                var gx = region.colorModel.getGreen(region.raster.getDataElements(i, j, null))
-                var bx = region.colorModel.getBlue(region.raster.getDataElements(i, j, null))
+        (0..region.width - 1).forEach { x ->
+            (0..region.height - 1).forEach { y ->
+                val ax = region.colorModel.getAlpha(region.raster.getDataElements(x, y, null))
+                var rx = region.colorModel.getRed(region.raster.getDataElements(x, y, null))
+                var gx = region.colorModel.getGreen(region.raster.getDataElements(x, y, null))
+                var bx = region.colorModel.getBlue(region.raster.getDataElements(x, y, null))
                 rx = (rx * r).toInt()
                 gx = (gx * g).toInt()
                 bx = (bx * b).toInt()
-                region.setRGB(i, j, (ax shl 24) or (rx shl 16) or (gx shl 8) or (bx shl 0))
+                if (ax < 50) {
+                    region.setRGB(x, y, textCharacter.getBackgroundColor().toAWTColor().rgb)
+                } else {
+                    region.setRGB(x, y, (ax shl 24) or (rx shl 16) or (gx shl 8) or (bx shl 0))
+                }
             }
         }
         return region
