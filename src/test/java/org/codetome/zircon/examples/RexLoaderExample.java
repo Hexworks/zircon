a8a157dbe4452e4776780c12505fda857fb030d2
adam-arold <arold.adam@gmail.com>
2017-09-20 18:08:06 +0200

    tileset example resize

--- src/test/java/org/codetome/zircon/examples/RexLoaderExample.java
+++ src/test/java/org/codetome/zircon/examples/RexLoaderExample.java
@@ -1,25 +1,24 @@
 package org.codetome.zircon.examples;
 
-import org.codetome.zircon.api.resource.CP437TilesetResource;
-import org.codetome.zircon.api.resource.REXPaintResource;
 import org.codetome.zircon.api.Size;
 import org.codetome.zircon.api.builder.TerminalBuilder;
 import org.codetome.zircon.api.graphics.Layer;
+import org.codetome.zircon.api.resource.CP437TilesetResource;
+import org.codetome.zircon.api.resource.REXPaintResource;
 import org.codetome.zircon.api.screen.Screen;
 
 import java.io.InputStream;
 import java.util.List;
 
 public class RexLoaderExample {
-    private static final int TERMINAL_WIDTH = 40;
-    private static final int TERMINAL_HEIGHT = 23;
+    private static final int TERMINAL_WIDTH = 16;
+    private static final int TERMINAL_HEIGHT = 16;
     private static final InputStream RESOURCE = RexLoaderExample.class.getResourceAsStream("/rex_files/cp437_table.xp");
 
     public static void main(String[] args) {
         REXPaintResource rex = REXPaintResource.loadREXFile(RESOURCE);
 
         final Screen screen = TerminalBuilder.newBuilder()
-//                .font(PhysicalFontResource.UBUNTU_MONO.toFont())
                 .font(CP437TilesetResource.TAFFER_20X20.toFont())
                 .initialTerminalSize(Size.of(TERMINAL_WIDTH, TERMINAL_HEIGHT))
                 .buildScreen();
