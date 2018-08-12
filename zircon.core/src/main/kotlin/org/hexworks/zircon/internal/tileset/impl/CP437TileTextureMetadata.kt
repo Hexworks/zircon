fcc9dda6502f010a2c870678247dbed2da9d4710
adam-arold <arold.adam@gmail.com>
2018-08-12 23:21:02 +0200

    fix symbols

--- zircon.core/src/main/kotlin/org/hexworks/zircon/internal/tileset/impl/CP437TileTextureMetadata.kt
+++ zircon.core/src/main/kotlin/org/hexworks/zircon/internal/tileset/impl/CP437TileTextureMetadata.kt
@@ -3,7 +3,7 @@ package org.hexworks.zircon.internal.tileset.impl
 import org.hexworks.zircon.api.data.CharacterTile
 import org.hexworks.zircon.api.tileset.TileTextureMetadata
 
-class CP437TileTextureMetadata(
+data class CP437TileTextureMetadata(
         override val x: Int,
         override val y: Int,
         override val width: Int,
---
+++ zircon.examples/src/main/kotlin/org/hexworks/zircon/examples/CustomTileExample.kt
@@ -0,0 +1,28 @@
+package org.hexworks.zircon.examples
+
+import org.hexworks.zircon.api.SwingApplications
+import org.hexworks.zircon.api.Tiles
+import org.hexworks.zircon.api.data.CharacterTile
+import org.hexworks.zircon.api.data.Position
+import org.hexworks.zircon.api.graphics.Symbols
+import org.hexworks.zircon.api.tileset.lookup.CP437TileMetadataLoader
+
+object CustomTileExample {
+
+    data class CustomTile(val characterTile: CharacterTile) : CharacterTile by characterTile
+
+    @JvmStatic
+    fun main(args: Array<String>) {
+
+        val tileGrid = SwingApplications.startTileGrid()
+
+        tileGrid.setTileAt(Position.defaultPosition(), CustomTile(Tiles.defaultTile().withCharacter('x')))
+
+        val loader = CP437TileMetadataLoader(16, 16)
+
+        val data = loader.fetchMetaForTile(Tiles.defaultTile().withCharacter(Symbols.INVERSE_BULLET))
+
+        println(data)
+    }
+
+}
