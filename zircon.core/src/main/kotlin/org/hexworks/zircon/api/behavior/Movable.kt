6c57a71eb84224456afe5416f6f491686695e034
adam-arold <arold.adam@gmail.com>
2018-09-06 23:53:55 +0200

    Add new Movable functions

--- zircon.core/src/main/kotlin/org/hexworks/zircon/api/behavior/Movable.kt
+++ zircon.core/src/main/kotlin/org/hexworks/zircon/api/behavior/Movable.kt
@@ -15,4 +15,14 @@ interface Movable : Positionable {
      * @return `true` if the position was changed `false` otherwise
      */
     fun moveTo(position: Position): Boolean
+
+    fun moveBy(position: Position) = moveTo(position() + position)
+
+    fun moveRightBy(delta: Int) = moveTo(position().withRelativeX(delta))
+
+    fun moveLeftBy(delta: Int) = moveTo(position().withRelativeX(-delta))
+
+    fun moveUpBy(delta: Int) = moveTo(position().withRelativeY(-delta))
+
+    fun moveDownBy(delta: Int) = moveTo(position().withRelativeY(delta))
 }
