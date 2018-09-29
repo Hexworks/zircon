e5913d72f46be25a1e3b5e556b2208a7be5f23f7
adam-arold <arold.adam@gmail.com>
2018-09-29 21:37:51 +0200

    fix retard test

--- zircon.jvm/src/test/kotlin/org/hexworks/zircon/api/input/InputTypeTest.kt
+++ zircon.jvm/src/test/kotlin/org/hexworks/zircon/api/input/InputTypeTest.kt
@@ -4,21 +4,23 @@ import org.assertj.core.api.Assertions.assertThat
 import org.hexworks.zircon.api.event.EventBus
 import org.hexworks.zircon.internal.event.ZirconEvent
 import org.junit.Test
+import java.util.concurrent.atomic.AtomicReference
 
 class InputTypeTest {
 
     @Test
     fun test() {
-        InputType.values().forEach {
-            val inputs = mutableListOf<Input>()
-            EventBus.subscribe<ZirconEvent.Input> { (input) ->
-                inputs.add(input)
+        InputType.values().forEach { inputType ->
+            val result = AtomicReference<Input>()
+            val subscription = EventBus.subscribe<ZirconEvent.Input> { (input) ->
+                result.set(input)
             }
             EventBus.broadcast(ZirconEvent.Input(KeyStroke(
                     character = ' ',
-                    type = it)))
-            assertThat(inputs.map { it.getInputType() }.first())
-                    .isEqualTo(it)
+                    type = inputType)))
+            assertThat(result.get().asKeyStroke().get().getInputType())
+                    .isEqualTo(inputType)
+            subscription.cancel()
         }
     }
 
