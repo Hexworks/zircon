e3506981e8ae159e0023ef088444f8866fd7482c
adam-arold <arold.adam@gmail.com>
2018-10-27 14:02:09 +0200

    Refurbish examples

---
+++ zircon.examples/src/main/java/org/hexworks/zircon/examples/docs/WorkingWithColorThemes.java
@@ -0,0 +1,48 @@
+package org.hexworks.zircon.examples.docs;
+
+import org.hexworks.zircon.api.*;
+import org.hexworks.zircon.api.component.ColorTheme;
+import org.hexworks.zircon.api.grid.TileGrid;
+import org.hexworks.zircon.api.screen.Screen;
+
+import java.util.Random;
+
+public class WorkingWithColorThemes {
+
+    public static void main(String[] args) {
+
+        final TileGrid tileGrid = SwingApplications.startTileGrid(
+                AppConfigs.newConfig()
+                        .withSize(Sizes.create(12, 10))
+                        .withDefaultTileset(CP437TilesetResources.rogueYun16x16())
+                        .build());
+
+        final Screen screen = Screens.createScreenFor(tileGrid);
+
+        screen.addComponent(Components.label()
+                .withText("Hello")
+                .withPosition(Positions.offset1x1())
+                .build());
+
+        screen.addComponent(Components.panel()
+                .withTitle("Panel")
+                .wrapWithBox(true)
+                .withPosition(Positions.create(1, 3))
+                .withSize(Sizes.create(10, 5))
+                .build());
+
+        ColorTheme custom = ColorThemes.newBuilder()
+                .withAccentColor(TileColors.fromString("#ff0000"))
+                .withPrimaryForegroundColor(TileColors.fromString("#ffaaff"))
+                .withSecondaryForegroundColor(TileColors.fromString("#dd88dd"))
+                .withPrimaryBackgroundColor(TileColors.fromString("#555555"))
+                .withSecondaryBackgroundColor(TileColors.fromString("#222222"))
+                .build();
+
+        ColorTheme builtIn = ColorThemes.adriftInDreams();
+
+        screen.applyColorTheme(new Random().nextInt(2) > 0 ? custom : builtIn);
+
+        screen.display();
+    }
+}
