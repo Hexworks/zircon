27210d7474a82aa93ece736f047102b1913ced8d
Luke Letourneau <lukeletourneau2000@gmail.com>
2019-10-31 19:30:12 -0400

    Modified LibGDX game system to fix compatibility with swing counterparts startApplication() and buildApplication()

--- zircon.jvm.libgdx/src/main/kotlin/org/hexworks/zircon/api/LibgdxApplications.kt
+++ zircon.jvm.libgdx/src/main/kotlin/org/hexworks/zircon/api/LibgdxApplications.kt
@@ -1,6 +1,5 @@
 package org.hexworks.zircon.api
 
-import com.badlogic.gdx.backends.lwjgl.LwjglApplication
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration
 import org.hexworks.zircon.api.application.AppConfig
 import org.hexworks.zircon.api.application.Application
@@ -14,32 +13,56 @@ object LibgdxApplications {
      * Builds a new [Application] using the given `appConfig`.
      */
     @JvmStatic
-    fun buildApplication(appConfig: AppConfig = AppConfigs.defaultConfiguration()): LibgdxApplication {
-        return LibgdxApplication(appConfig)
+    @JvmOverloads
+    fun buildApplication(appConfig: AppConfig = AppConfigs.defaultConfiguration(),
+                         libgdxConfig: LwjglApplicationConfiguration = LwjglApplicationConfiguration()): LibgdxApplication {
+        return makeLibgdxGame(appConfig, libgdxConfig).libgdxApplication
     }
 
     /**
      * Builds and starts a new [Application] from the given `appConfig`.
      */
     @JvmStatic
-    fun startApplication(appConfig: AppConfig = AppConfigs.defaultConfiguration()): LibgdxApplication {
-        return startLibgdxGame(appConfig).libgdxApplication
+    @JvmOverloads
+    fun startApplication(appConfig: AppConfig = AppConfigs.defaultConfiguration(),
+                         libgdxConfig: LwjglApplicationConfiguration = LwjglApplicationConfiguration()): LibgdxApplication {
+        with(makeLibgdxGame(appConfig, libgdxConfig)) {
+            start()
+            return this.libgdxApplication
+        }
+    }
+
+    /**
+     * Builds JUST a new [Application], not a libgdx game
+     */
+    @JvmStatic
+    @JvmOverloads
+    fun buildRawApplication(appConfig: AppConfig = AppConfigs.defaultConfiguration()): LibgdxApplication {
+        return LibgdxApplication(appConfig)
+    }
+
+    /**
+     * Starts JUST a new [Application], not a libgdx game
+     */
+    @JvmStatic
+    @JvmOverloads
+    fun startRawApplication(appConfig: AppConfig = AppConfigs.defaultConfiguration()): LibgdxApplication {
+        return LibgdxApplication(appConfig).also {
+            it.start()
+        }
     }
 
     /**
      * Builds and starts a new [Application] and returns its [TileGrid].
      */
     @JvmStatic
-    fun startTileGrid(): TileGrid = startTileGrid(AppConfigs.defaultConfiguration())
-
-    /**
-     * Builds and starts a new [Application] and returns its [TileGrid].
-     */
-    @JvmStatic
-    fun startTileGrid(appConfig: AppConfig = AppConfigs.defaultConfiguration()): TileGrid {
+    @JvmOverloads
+    fun startTileGrid(appConfig: AppConfig = AppConfigs.defaultConfiguration(),
+                      libgdxConfig: LwjglApplicationConfiguration = LwjglApplicationConfiguration()): TileGrid {
         val maxTries = 10
         var currentTryCount = 0
-        val game = startLibgdxGame(appConfig)
+        val game = makeLibgdxGame(appConfig, libgdxConfig)
+        game.start()
         var notInitialized = true
         while (notInitialized) {
             try {
@@ -57,16 +80,21 @@ object LibgdxApplications {
         return game.libgdxApplication.tileGrid
     }
 
-    private fun startLibgdxGame(appConfig: AppConfig = AppConfigs.defaultConfiguration()): LibgdxGame {
-        val config = LwjglApplicationConfiguration()
-        config.title = appConfig.title
-        config.width = appConfig.size.width * appConfig.defaultTileset.width
-        config.height = appConfig.size.height * appConfig.defaultTileset.height
-        config.foregroundFPS = appConfig.fpsLimit
-        config.useGL30 = false
-        val game = LibgdxGame(appConfig)
-        LwjglApplication(game, config)
-        return game
+    /**
+     * Creates a [LibgdxGame] that contains a built and started [Application]
+     *
+     * Note that the Zircon `appConfig` fields title, size, and fpsLimit
+     * will override the Libgdx `LwjglApplicationConfiguration` fields
+     * title, width and height, and foregroundFPS respectively.
+     */
+    @JvmStatic
+    private fun makeLibgdxGame(appConfig: AppConfig = AppConfigs.defaultConfiguration(),
+                                libgdxConfig: LwjglApplicationConfiguration = LwjglApplicationConfiguration()): LibgdxGame {
+        libgdxConfig.title = appConfig.title
+        libgdxConfig.width = appConfig.size.width * appConfig.defaultTileset.width
+        libgdxConfig.height = appConfig.size.height * appConfig.defaultTileset.height
+        libgdxConfig.foregroundFPS = appConfig.fpsLimit
+        libgdxConfig.useGL30 = false
+        return LibgdxGame.build(appConfig, libgdxConfig)
     }
-
 }
