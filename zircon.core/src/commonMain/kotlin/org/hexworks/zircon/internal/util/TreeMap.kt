08af107ae8d601a82e05d80a57e9e6b004e04add
Adam Arold <arold.adam@gmail.com>
2019-11-30 01:31:26 +0100

    Remove TreeMap

--- zircon.core/src/commonMain/kotlin/org/hexworks/zircon/internal/util/TreeMap.kt
+++
@@ -1,6 +0,0 @@
-package org.hexworks.zircon.internal.util
-
-interface TreeMap<K, V> : MutableMap<K, V> {
-
-    fun getOrDefault(key: K, defaultValue: V): V
-}
--- zircon.core/src/commonMain/kotlin/org/hexworks/zircon/platform/factory/TreeMapFactory.kt
+++
@@ -1,8 +0,0 @@
-package org.hexworks.zircon.platform.factory
-
-import org.hexworks.zircon.internal.util.TreeMap
-
-expect object TreeMapFactory {
-
-    fun <K, V> create(): TreeMap<K, V>
-}
