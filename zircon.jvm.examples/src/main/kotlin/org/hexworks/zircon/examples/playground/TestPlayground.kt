9519fbaaebbcfe690ae5d65eb8ab601400d37253
Adam Arold <arold.adam@gmail.com>
2020-10-02 19:40:30 +0200

    Add test implementation for the new concurrent tree structure

--- zircon.jvm.examples/src/main/kotlin/org/hexworks/zircon/examples/playground/TestPlayground.kt
+++ zircon.jvm.examples/src/main/kotlin/org/hexworks/zircon/examples/playground/TestPlayground.kt
@@ -2,137 +2,229 @@
 
 package org.hexworks.zircon.examples.playground
 
-import kotlinx.collections.immutable.persistentMapOf
+import kotlinx.collections.immutable.PersistentList
+import kotlinx.collections.immutable.persistentListOf
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
 import kotlinx.coroutines.channels.actor
+import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
-import org.hexworks.zircon.api.data.Position
-import org.hexworks.zircon.api.data.Tile
-import java.text.DecimalFormat
-import java.text.NumberFormat
-import java.util.*
-import java.util.concurrent.CountDownLatch
-import java.util.concurrent.Executors
-import java.util.concurrent.TimeUnit
-import kotlin.random.Random
-import kotlin.system.measureNanoTime
-
+import org.hexworks.zircon.examples.playground.TestPlayground.ConcurrentTree.Message.AddChildTo
+import kotlin.math.max
 
 object TestPlayground {
 
+    interface Tree<D : Any> {
 
-    interface Layer {
+        val descendants: Sequence<TreeNode<D>>
+        val children: Sequence<TreeNode<D>>
 
-        val contents: Map<Position, Tile>
+        fun createChild(data: D? = null): TreeNode<D>
 
-        fun setTileAt(position: Position, tile: Tile)
+        companion object {
+
+            fun <D : Any> create(): Tree<D> {
+                return ConcurrentTree()
+            }
+        }
     }
 
-    class ThreadSafeLayer : Layer {
+    interface TreeNode<D : Any> {
+        val data: D?
+        val parent: TreeNode<D>?
+        val hasParent: Boolean
+            get() = parent != null
+        val children: Sequence<TreeNode<D>>
 
-        override var contents = persistentMapOf<Position, Tile>()
-            @Synchronized
-            get
+        fun createChild(data: D? = null): TreeNode<D>
 
-        @Synchronized
-        override fun setTileAt(position: Position, tile: Tile) {
-            contents = contents.put(position, tile)
+        fun remove()
+    }
+
+    @Suppress("UNCHECKED_CAST")
+    class ConcurrentTree<D : Any> : Tree<D> {
+
+        sealed class Message {
+
+            data class AddChildTo<D : Any>(
+                    val parent: TreeNode<D>,
+                    val child: TreeNode<D>
+            ) : Message()
+
+            data class DeleteChild<D : Any>(
+                    val child: TreeNode<D>
+            ) : Message()
         }
 
-    }
+        private val treeScope = CoroutineScope(Dispatchers.Default + SupervisorJob())
 
-    sealed class LayerMsg {
-
-        data class SetTileAt(val position: Position,
-                             val tile: Tile) : LayerMsg()
-
-    }
-
-    class ActorLayer : Layer, CoroutineScope {
-
-        override val coroutineContext = Dispatchers.Default
-
-        override var contents = persistentMapOf<Position, Tile>()
-            private set
-
-        private val actor = actor<LayerMsg> {
-            var counter = 0 // actor state
+        private val actor = treeScope.actor<Message> {
             for (msg in channel) { // iterate over incoming messages
                 when (msg) {
-                    is LayerMsg.SetTileAt -> {
-                        contents = contents.put(msg.position, msg.tile)
+                    is AddChildTo<*> -> {
+                        val (parent, child) = msg
+                        val lastChildIdx = backend.indexOfLast { it.parent == parent }
+                        val parentIdx = backend.indexOf(parent)
+                        val idx = max(lastChildIdx, parentIdx) + 1
+                        backend = if (idx == backend.lastIndex) {
+                            backend.add(child as TreeNode<D>)
+                        } else {
+                            backend.add(idx, child as TreeNode<D>)
+                        }
+                    }
+                    is Message.DeleteChild<*> -> {
+                        val parent = msg.child.parent
+                        val child = msg.child
+                        val toDelete = backend
+                                .dropWhile { it !== child }
+                                .takeWhile { it === child || it.parent !== parent }
+                        backend = backend.removeAll(toDelete)
                     }
                 }
             }
         }
 
-        override fun setTileAt(position: Position, tile: Tile) {
-            runBlocking {
-                actor.send(LayerMsg.SetTileAt(position, tile))
+        private val root = RootNode(
+                parent = null,
+                data = null,
+                tree = this
+        )
+        private var backend: PersistentList<TreeNode<D>> = persistentListOf(root)
+
+        override val descendants: Sequence<TreeNode<D>>
+            get() = backend.asSequence().drop(1)
+
+        override val children: Sequence<TreeNode<D>>
+            get() = root.children
+
+        override fun createChild(data: D?) = root.createChild(data)
+
+        fun addChildTo(parent: TreeNode<D>, child: TreeNode<D>) {
+            treeScope.launch {
+                actor.send(AddChildTo(parent, child))
             }
         }
+
+        fun remove(node: TreeNode<D>) {
+            treeScope.launch {
+                actor.send(Message.DeleteChild(node))
+            }
+        }
+
+        override fun toString(): String {
+            return "Tree(descendants=${descendants.joinToString()})"
+        }
+    }
+
+    open class ConcurrentTreeNode<D : Any>(
+            override val parent: ConcurrentTreeNode<D>? = null,
+            override val data: D? = null,
+            private val tree: ConcurrentTree<D>
+    ) : TreeNode<D> {
+
+        override val children: Sequence<TreeNode<D>>
+            get() = tree.children.filter { it.parent == this }
+
+        override fun createChild(data: D?): ConcurrentTreeNode<D> {
+            val parent = this
+            return ConcurrentTreeNode(
+                    parent = this,
+                    data = data,
+                    tree = tree
+            ).apply {
+                tree.addChildTo(parent, this)
+            }
+        }
+
+        override fun remove() {
+            tree.remove(this)
+        }
+
+        override fun toString(): String {
+            return "TreeNode(data=$data)"
+        }
+    }
+
+    private class RootNode<D : Any>(
+            parent: ConcurrentTreeNode<D>? = null,
+            data: D? = null,
+            tree: ConcurrentTree<D>
+    ) : ConcurrentTreeNode<D>(
+            parent = parent,
+            data = data,
+            tree = tree
+    ) {
+        override fun remove() {
+            throw RuntimeException("A root node can't be removed.")
+        }
+    }
+
+    @JvmStatic
+    fun main(args: Array<String>) = runBlocking {
+
+        simpleUsage()
+        nestedUsage()
+        deleteSubtree()
+        deleteLeaf()
 
     }
 
+    private fun simpleUsage() {
+        val tree = ConcurrentTree<String>()
 
-    @JvmStatic
-    fun main(args: Array<String>) {
+        val child0 = tree.createChild("0")
+        val child1 = tree.createChild("1")
 
-        val testsToRun = 10
+        Thread.sleep(500)
 
-        val tiles = listOf(
-                Tile.defaultTile().withCharacter('a'),
-                Tile.defaultTile().withCharacter('b'),
-                Tile.defaultTile().withCharacter('c'))
-        val tileCount = tiles.size
+        println(tree.descendants.map { it.data }.joinToString())
+    }
 
-        val random = Random(65486)
-        val writesToDo = 1000000
+    private fun nestedUsage() {
+        val tree = ConcurrentTree<String>()
 
-        val reader = Executors.newFixedThreadPool(1)
-        val writer = Executors.newFixedThreadPool(1)
+        val child0 = tree.createChild("0")
+        val child00 = child0.createChild("00")
+        val child1 = tree.createChild("1")
+        val child10 = child1.createChild("10")
+        val child11 = child1.createChild("11")
 
-        val formatter = NumberFormat.getInstance(Locale.US) as DecimalFormat
-        val symbols = formatter.decimalFormatSymbols
-        symbols.groupingSeparator = ' '
-        formatter.decimalFormatSymbols = symbols
+        Thread.sleep(500)
 
-        repeat(testsToRun) {
+        println(tree.descendants.map { it.data }.joinToString())
+    }
 
-            val layer = ActorLayer()
-            val latch = CountDownLatch(writesToDo)
-            var reads = 0
+    private fun deleteSubtree() {
+        val tree = ConcurrentTree<String>()
 
-            val timeNs = measureNanoTime {
-                var running = true
-                reader.submit {
-                    while (running) {
-                        layer.contents
-                        reads++
-                    }
-                }
-                writer.submit {
-                    while (running) {
-                        layer.setTileAt(
-                                position = Position.create(random.nextInt(1000), random.nextInt(1000)),
-                                tile = tiles[random.nextInt(tileCount)])
-                        latch.countDown()
-                    }
-                }
-                latch.await(1, TimeUnit.MINUTES)
-                running = false
-            }
+        val child0 = tree.createChild("0")
+        val child00 = child0.createChild("00")
+        val child1 = tree.createChild("1")
+        val child10 = child1.createChild("10")
+        val child11 = child1.createChild("11")
 
-            val totalMs = timeNs.toDouble().div(1000).div(1000)
-            val wps = writesToDo.div(totalMs.div(1000))
+        child1.remove()
 
-            println("Benchmark ran in ${formatter.format(totalMs.toLong())}ms." +
-                    " Reads: ${formatter.format(reads)}." +
-                    " Writes: ${formatter.format(writesToDo)}." +
-                    " WPS: ${formatter.format(wps.toLong())}/s.")
-        }
+        Thread.sleep(500)
 
+        println(tree.descendants.map { it.data }.joinToString())
+    }
+
+    private fun deleteLeaf() {
+        val tree = ConcurrentTree<String>()
+
+        val child0 = tree.createChild("0")
+        val child00 = child0.createChild("00")
+        val child1 = tree.createChild("1")
+        val child10 = child1.createChild("10")
+        val child11 = child1.createChild("11")
+
+        child11.remove()
+
+        Thread.sleep(500)
+
+        println(tree.descendants.map { it.data }.joinToString())
     }
 
 }
