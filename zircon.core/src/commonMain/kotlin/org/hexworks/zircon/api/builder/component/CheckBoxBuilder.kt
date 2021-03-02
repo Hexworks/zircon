ac76ed2c2db57b7756d5675760e65e106e3f48e8
Abbi <33093801+lesbiangunshow@users.noreply.github.com>
2021-03-02 20:43:01 +0000

    hopefully the last of this lol

--- zircon.core/src/commonMain/kotlin/org/hexworks/zircon/api/builder/component/CheckBoxBuilder.kt
+++ zircon.core/src/commonMain/kotlin/org/hexworks/zircon/api/builder/component/CheckBoxBuilder.kt
@@ -2,7 +2,6 @@ package org.hexworks.zircon.api.builder.component
 
 import org.hexworks.zircon.api.component.CheckBox
 import org.hexworks.zircon.api.component.builder.base.BaseComponentBuilder
-import org.hexworks.zircon.api.data.Size
 import org.hexworks.zircon.internal.component.impl.DefaultCheckBox
 import org.hexworks.zircon.internal.component.impl.DefaultCheckBox.CheckBoxAlignment
 import org.hexworks.zircon.internal.component.renderer.DefaultCheckBoxRenderer
@@ -10,19 +9,30 @@ import kotlin.jvm.JvmStatic
 import kotlin.math.max
 
 @Suppress("UNCHECKED_CAST")
+
 class CheckBoxBuilder(
     private var text: String = "",
     private var labelAlignment: CheckBoxAlignment = CheckBoxAlignment.RIGHT
 ) : BaseComponentBuilder<CheckBox, CheckBoxBuilder>(DefaultCheckBoxRenderer()) {
 
     init {
-        contentSize = Size.create(text.length + DefaultCheckBoxRenderer.BUTTON_WIDTH, 1)
+        val totalSize =
+            if (text == "")
+                DefaultCheckBoxRenderer.BUTTON_WIDTH
+            else
+                text.length + DefaultCheckBoxRenderer.DECORATION_WIDTH
+        contentSize = contentSize.withWidth(totalSize)
     }
 
     fun withText(text: String) = also {
         this.text = text
+        val totalSize =
+            if (text == "")
+                DefaultCheckBoxRenderer.BUTTON_WIDTH
+            else
+                text.length + DefaultCheckBoxRenderer.DECORATION_WIDTH
         contentSize = contentSize
-            .withWidth(max(text.length + DefaultCheckBoxRenderer.BUTTON_WIDTH, contentSize.width))
+            .withWidth(max(totalSize, contentSize.width))
     }
 
     fun withLeftAlignedText() = also {
