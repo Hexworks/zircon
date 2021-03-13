b4d9af77eaebe24a1c0672697599421c014bbfd6
Max A <nanodeath@gmail.com>
2021-03-13 09:53:48 -0800

    Introduce TestRenderer, a renderer for use in headless tests.

---
+++ zircon.core/src/jvmTest/kotlin/org/hexworks/zircon/internal/renderer/TestRendererTest.kt
@@ -0,0 +1,45 @@
+package org.hexworks.zircon.internal.renderer
+
+import org.assertj.core.api.Assertions.assertThat
+import org.hexworks.zircon.api.ComponentDecorations.box
+import org.hexworks.zircon.api.Components
+import org.hexworks.zircon.api.DrawSurfaces
+import org.hexworks.zircon.api.data.Size
+import org.hexworks.zircon.api.graphics.BoxType
+import org.hexworks.zircon.convertCharacterTilesToString
+import org.junit.Test
+
+class TestRendererTest {
+    @Test
+    fun tinyExample() {
+        val graphics = DrawSurfaces.tileGraphicsBuilder().withSize(Size.create(3, 1)).build()
+        val testRenderer = TestRenderer(graphics).apply {
+            withComponentContainer {
+                addComponent(Components.textBox(3).addParagraph("Foo").build())
+            }
+        }
+        testRenderer.render()
+        assertThat(graphics.convertCharacterTilesToString()).isEqualTo("Foo")
+    }
+
+    @Test
+    fun rendersAsExpected() {
+        val text = "Hello Zircon"
+        val graphics = DrawSurfaces.tileGraphicsBuilder().withSize(Size.create(text.length + 2, 4)).build()
+        val testRenderer = TestRenderer(graphics).apply {
+            withComponentContainer {
+                addComponent(Components.textBox(text.length)
+                    .withDecorations(box(boxType = BoxType.TOP_BOTTOM_DOUBLE))
+                    .addParagraph(text, withNewLine = false)
+                    .build())
+            }
+        }
+        testRenderer.render()
+        assertThat(graphics.convertCharacterTilesToString()).isEqualTo("""
+            ╒════════════╕
+            │Hello Zircon│
+            │            │
+            ╘════════════╛
+        """.trimIndent())
+    }
+}
