360acde2f83fa6520fa4c861ef5b1a3aee45355e
Adam Arold <arold.adam@gmail.com>
2021-08-30 11:44:02 +0200

    Fix hiding transformer

--- zircon.jvm.examples/src/main/kotlin/org/hexworks/zircon/examples/playground/KotlinPlayground.kt
+++ zircon.jvm.examples/src/main/kotlin/org/hexworks/zircon/examples/playground/KotlinPlayground.kt
@@ -2,25 +2,36 @@
 
 package org.hexworks.zircon.examples.playground
 
-import org.hexworks.zircon.api.ColorThemes
-import org.hexworks.zircon.api.SwingApplications
-import org.hexworks.zircon.api.builder.component.ParagraphBuilder
-import org.hexworks.zircon.api.extensions.toScreen
+import org.hexworks.zircon.api.color.ANSITileColor
+import org.hexworks.zircon.api.data.Position
+import org.hexworks.zircon.api.data.Size
+import org.hexworks.zircon.api.data.Tile
+import org.hexworks.zircon.api.graphics.Layer
+import org.hexworks.zircon.api.modifier.SimpleModifiers
+import org.hexworks.zircon.examples.base.displayScreen
 
 object KotlinPlayground {
 
     @JvmStatic
     fun main(args: Array<String>) {
 
-        SwingApplications.startTileGrid().toScreen().apply {
-            display()
-            theme = ColorThemes.arc()
-            addComponent(
-                ParagraphBuilder.newBuilder()
-                    .withText("Some text I added")
-                    .withTypingEffect(200)
-            )
-        }
+        val screen = displayScreen()
+
+        val transformingLayer = Layer.newBuilder()
+            .withSize(Size.create(20, 20))
+            .withOffset(Position.create(1, 5))
+            .build().apply {
+                fill(
+                    Tile.newBuilder()
+                        .withBackgroundColor(ANSITileColor.RED)
+                        .withForegroundColor(ANSITileColor.GREEN)
+                        .withCharacter('x')
+                        .withModifiers(SimpleModifiers.Hidden)
+                        .buildCharacterTile()
+                )
+            }
+
+        screen.addLayer(transformingLayer)
 
     }
 
