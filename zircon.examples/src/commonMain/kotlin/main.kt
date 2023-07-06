30549824097fe38649b7b65e833c7200769b3aac
Adam Arold <arold.adam@gmail.com>
2023-07-06 14:22:24 +0200

    progress

--- zircon.examples/src/commonMain/kotlin/main.kt
+++ zircon.examples/src/commonMain/kotlin/main.kt
@@ -12,22 +12,24 @@ suspend fun main() {
     var currIdx = 0
     val random = Random(13513516895)
     val tileSize = 8
-    val gridWidth = 1920 / tileSize
-    val gridHeight = 1080 / tileSize
+    val gridCols = 1920 / tileSize
+    val gridRows = 1080 / tileSize
 
     fun ANSITileColor.Companion.random(): ANSITileColor =
         ANSITileColor.values()[random.nextInt(0, ANSITileColor.values().size)]
 
     val app = Applications.createApplication(appConfig {
-        size = Size.create(gridWidth, gridHeight)
+        size = Size.create(gridCols, gridRows)
         defaultTileset = BuiltInCP437TilesetResource.valueOf("REX_PAINT_${tileSize}X${tileSize}")
         debugMode = true
     })
     val grid = app.tileGrid
 
+    println("grid size is: ${grid.size}")
+
     app.beforeRender {
-        for (x in 0 until gridWidth) {
-            for (y in 1 until gridHeight) {
+        for (x in 0 until gridCols) {
+            for (y in 1 until gridRows) {
                 grid.draw(
                     Tile.newBuilder()
                         .withCharacter(CP437Utils.convertCp437toUnicode(random.nextInt(0, 255)))
